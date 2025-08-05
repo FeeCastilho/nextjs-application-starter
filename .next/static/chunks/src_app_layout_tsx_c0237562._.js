2edaf74eb1454a96ce3b42fa3ebd7314c20e8a88
FeeCastilho <166766615+FeeCastilho@users.noreply.github.com>
2025-08-04 22:46:11 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1754358060801

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__404369a3._.css",
+  "static/chunks/node_modules_5c3e905e._.js",
+  "static/chunks/src_f76cb561._.js"
+],
+    source: "dynamic"
+});
