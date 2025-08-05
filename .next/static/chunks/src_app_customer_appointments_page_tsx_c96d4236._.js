5bbbf1c169b5ee1bf8a62baef385b0bc4ce2648b
FeeCastilho <166766615+FeeCastilho@users.noreply.github.com>
2025-08-04 22:46:09 -0300

    Add .next/static/chunks/src_app_customer_appointments_page_tsx_c96d4236._.js 1754358058851

---
+++ .next/static/chunks/src_app_customer_appointments_page_tsx_c96d4236._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7e43de8e._.js",
+  "static/chunks/src_4dcc12af._.js"
+],
+    source: "dynamic"
+});
